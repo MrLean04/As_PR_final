c2d8114c17dc1f1298f19895c0646b9489f42af1
Ana Filipe <72576056+AnaaFilipe@users.noreply.github.com>
2021-06-17 10:11:55 +0100

    Update
    Lingua e centrar

--- app/src/main/java/com/example/smartparkingapp/MainActivity.java
+++ app/src/main/java/com/example/smartparkingapp/MainActivity.java
@@ -7,16 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonSignIn, buttonSignUp;
+    private TextView txtWelTxt,txtSignIn, txtWelcome, txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initialmenu);
+
+        txtWelcome = findViewById(R.id.textView);
+        txtWelcome.setText(getString(R.string.welcome));
+
+        txtWelTxt = findViewById(R.id.textView2);
+        txtWelTxt.setText(getString(R.string.welTxt));
+
+        txtWelTxt = findViewById(R.id.textView2);
+        txtWelTxt.setText(getString(R.string.welTxt));
+
+        txtSignIn = findViewById(R.id.button_signin);
+        txtSignIn .setText(getString(R.string.signin_button));
+
+        txtSignUp = findViewById(R.id.button_signup);
+        txtSignUp .setText(getString(R.string.signup_button));
 
         buttonSignIn = findViewById(R.id.button_signin);
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
